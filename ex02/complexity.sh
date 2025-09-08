df33fe026ddc9e26b9e8faf8b54d3f49a92d41eb
Gaelle Ozon <gozon@student.42.fr>
2025-09-08 11:07:14 +0200

    Fix column

--- ex02/complexity.sh
+++ ex02/complexity.sh
@@ -4,10 +4,10 @@ runs=100
 sum=0
 max=0
 count=0
-size=21
+size=32
 
 for i in $(seq 1 $runs); do
-    cmp=$(./PmergeMe $(shuf -i 1-1000 -n $size | tr "\n" " ") | grep "Comparisons:" | awk '{print $3}')
+    cmp=$(./PmergeMe $(shuf -i 1-1000 -n $size | tr "\n" " ") | grep "Comparisons:" | awk '{print $2}')
 
     echo "Run $i: [$cmp]"
 	((count++))
